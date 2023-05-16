17a928cdb3b7bac11b8de4d0a35408fd828a960c
Manami <manami2622@gmail.com>
2023-05-16 17:52:32 +0900

    done task1; test case yet

--- week1/task1/findAnagram.py
+++ week1/task1/findAnagram.py
@@ -1,6 +1,75 @@
 
+from pathlib import Path
 
+def sortDictionary(filePath):
+    original_dic = []
+    with open(filePath) as f:
+        lines = f.readlines()
+        for l in lines:
+            original_dic.append(l.rstrip("\n"))
+        
+    new_dic = {}
+
+    for original_word in original_dic:
+        sorted_word = "".join(sorted(original_word))
+        if sorted_word in new_dic:
+            new_dic[sorted_word].append(original_word)
+        else:
+            new_dic[sorted_word] = [ original_word ]
+
+    sorted_dic = dict(sorted(new_dic.items())) # sort words alphabetically
+    return sorted_dic
+
+
+def findKeyByBinarySearch(sorted_targetWord, sorted_dic):
+    
+    keys = list(sorted_dic.keys())
+    right = len(keys) -1
+    left = 0
+    
+    while(left <= right):
+        mid = left + (right-left)//2
+        if keys[mid] == sorted_targetWord:
+            return sorted_dic[keys[mid]]
+        
+        elif keys[mid] < sorted_targetWord:
+            left = mid + 1
+        else :
+            right = mid - 1 
+    
+ 
+    return []
+   
+def solveTask1(targeWord):
+        
+    # 現在ディレクトリのパスを取得
+    current_dir_path = Path(__file__).resolve().parent
+
+    # words.txt
+    words_path = current_dir_path / Path('../txt_files/words.txt')
+
+    # sort dictionary
+    sorted_dic = sortDictionary(words_path)
+    
+    # input
+  
+    sorted_targetWord = "".join(sorted(targetWord))
+    
+    # binary search 
+    value = findKeyByBinarySearch(sorted_targetWord, sorted_dic)
+    
+    if len(value) > 0:
+        print("The word can be anagram of "+ str(value))
+    else:
+        print("Anagram Not Found")
+    
+    return
 
 
 if __name__ == "__main__":
-    string = input("Anagramを入力してください")
+  
+    # input
+    targetWord = input("英数字を入力してください: ")
+    
+    solveTask1(targetWord)
+   
