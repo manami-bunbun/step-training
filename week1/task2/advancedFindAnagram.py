a1f3e16750a13823d61be7b1757c712cf0578f6c
Manami <manami2622@gmail.com>
2023-05-19 01:37:52 +0900

    wrong score

--- week1/task2/advancedFindAnagram.py
+++ week1/task2/advancedFindAnagram.py
@@ -1,38 +1,30 @@
 from collections import Counter
 from pathlib import Path
 
-def makeDictionary(filePath):
-    new_dic = {}
-    count_dic = {}
+
+# word.txt -> sort tupples[("abc", (Counter(word)))] -----------------
+def makeDictionary(filePath):    
+    count_list = []
     
     with open(filePath) as f:
         lines = f.readlines()
         for l in lines:
             original_word = l.rstrip("\n")
-            sorted_word = "".join(sorted(original_word))
-            if sorted_word in new_dic:
-                new_dic[sorted_word].append(original_word)
-            else:
-                new_dic[sorted_word] = [ original_word ]
-                count_dic[original_word] = Counter(original_word)   
-    return new_dic, count_dic
+            count_list.append((original_word, Counter(original_word)))       
+    count_list = sorted(count_list, key=lambda x: len(x[0]), reverse=True)
+    count_list = sorted(count_list, key=lambda x: calculateOneWordScore(x[1]), reverse=True)
+       
+    return count_list
 
-def is_subset(A, count_dic):
-    valid_lists = []
-    for word, hashmap in count_dic.items():
-        is_subset = all(A[char] >= hashmap[char] for char in hashmap)
-        if is_subset:
-            valid_lists.append(word)
-    return valid_lists
 
-def calculateScores(subsetAnagrams):
-    SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4]
-    scores = []
-    for anagram in subsetAnagrams:
-        score = sum(SCORES[ord(char) - ord('a')] for char in anagram)
-        scores.append((score, anagram))
-    return max(scores)[1]
+def calculateOneWordScore(oneWord):
+    SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4]    
+    score = sum(SCORES[ord(char) - ord('a')] for char in oneWord)
+    return score
 
+# --------------------------------------------------------------
+
+# small.txt -> dictionary
 def countCharacter(filePath):
     target_dic = {}
     
@@ -43,6 +35,21 @@ def countCharacter(filePath):
             target_dic[original_word] = Counter(original_word)
     return target_dic
 
+
+# ------------------------------------------------------------
+
+# find a highest score from sorted tupples
+def is_subset(A, count_list):
+    for word, hashmap in count_list:
+        is_subset = all(A[char] >= hashmap[char] for char in hashmap)
+        if is_subset:
+            return word
+    
+
+# --------------------------------------------------------------
+
+
+
 def solveTask2(inputFilename, outputFilename):
     # 現在ディレクトリのパスを取得
     current_dir_path = Path(__file__).resolve().parent
@@ -51,7 +58,7 @@ def solveTask2(inputFilename, outputFilename):
     words_path = current_dir_path / Path('../txt_files/words.txt')
 
     # sort dictionary
-    sorted_dic, count_dic = makeDictionary(words_path)
+    count_list = makeDictionary(words_path)
     
     input_path = current_dir_path / Path('../txt_files/') / inputFilename
 
@@ -60,16 +67,11 @@ def solveTask2(inputFilename, outputFilename):
 
     # input
     target_dic = countCharacter(input_path)
-
-    # anagramCandidates
-    all_valid_lists = []
-    # compare hashmap to find valid word patterns
-    for i in target_dic.values():
-        all_valid_lists.append(is_subset(i, count_dic))
-        
+    
     with open(output_path,'w') as f_ans:
-        for k in all_valid_lists:
-            f_ans.write(calculateScores(k)+"\n")
+        # compare hashmap to find valid word patterns
+        for i in target_dic.values():
+            f_ans.write(is_subset(i, count_list)+"\n")
     return
 
 
