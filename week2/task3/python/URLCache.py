16d67e527d5d5dd08520271774519c00365d6031
Manami <manami2622@gmail.com>
2023-05-25 20:16:16 +0900

    task3; python done

--- week2/task3/python/URLCache.py
+++ week2/task3/python/URLCache.py
@@ -1,67 +1,64 @@
 from node import Node
 
 class URLCache:
-    def __init__(self,capacity):
-       self.capacity = capacity
-       self.map ={}
-       self.head = None
-       self.tail = None
-    
-    def get(self,url):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.map = {}
+        self.head = None
+        self.tail = None
+
+    def get(self, url):
         if url in self.map:
             node = self.map[url]
             self.remove(node)
             self.setHead(node)
             return node.webPage
-        else:
-            return None
-    
+        return None
+
     def set(self, url, webPage):
         if url in self.map:
-            node = self.map[url]
-            node.webPage = webPage
-            self.remove(node)
-            self.setHead(node)
+            old = self.map[url]
+            old.webPage = webPage
+            self.remove(old)
+            self.setHead(old)
         else:
-            node = Node(url,webPage)
+            created = Node(url, webPage)
             if len(self.map) >= self.capacity:
                 del self.map[self.tail.url]
                 self.remove(self.tail)
-                self.setHead(node)
+                self.setHead(created)
             else:
-                self.setHead(node)
-            self.map[url] = node
-    
+                self.setHead(created)
+            self.map[url] = created
+
     def remove(self, node):
-        if(node.prev != None):
+        if node.prev is not None:
             node.prev.next = node.next
         else:
             self.head = node.next
-            
-        if(node.next != None):
+
+        if node.next is not None:
             node.next.prev = node.prev
         else:
             self.tail = node.prev
-        
-    
+
     def setHead(self, node):
         node.next = self.head
         node.prev = None
-        
-        if self.head != None:
+
+        if self.head is not None:
             self.head.prev = node
-        
-        head = node
-        
-        if self.tail == None:
+
+        self.head = node
+
+        if self.tail is None:
             self.tail = self.head
-            
+
     def printCache(self):
-        currentNode = self.head 
-        while currentNode != None:
-            print([currentNode.url,currentNode.webPage])
+        currentNode = self.head
+        while currentNode is not None:
+            print(f"[{currentNode.url}, {currentNode.webPage}]", end="")
             currentNode = currentNode.next
-            if currentNode != None:
-                print("->")
-        print("\n")
-        
+            if currentNode is not None:
+                print(" -> ", end="")
+        print()
